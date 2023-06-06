16d77cc9997fa37d497966a1d480af02a339c628
Shrey K <srkkadia@gmail.com>
2023-06-06 12:46:58 +0530

    Session Techniques: Hidden Form Field / URLRewritting

---
+++ First_App/src/HFF_Servlet_2.java
@@ -0,0 +1,32 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class HFF_Servlet_2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public HFF_Servlet_2() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		res.setContentType("text/html");
+		PrintWriter out = res.getWriter();
+		
+		String name = req.getParameter("uname");
+		out.print("Hello " + name);
+		out.close();
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
